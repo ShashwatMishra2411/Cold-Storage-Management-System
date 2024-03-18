262889879e69e7340acf557aef2b4477bbe17a1c
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-03-18 22:35:58 +0530

    🐛 Update routes in app.js

--- Server/app.js
+++ Server/app.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/customers', require('./Customers/routes/auth'));
+app.use('/customers', require('./Customers/routes/userRoutes'));
 
-app.use('/owners', require('./Owners/routes/userRoutes'));
+// app.use('/owners', require('./Owners/routes/userRoutes'));
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000...');
