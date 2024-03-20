1556a65df123f04647d73b45103f9522869f8ab6
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-03-20 18:59:16 +0530

    ✨ Refactor userRoutes.js to use updated middleware and add chamber selection middleware

--- Server/Customers/routes/userRoutes.js
+++ Server/Customers/routes/userRoutes.js
@@ -1,53 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const middleware = require("../middleware/auth");
+const auth = require("../middleware/auth");
+const chambers = require("../middleware/chamberSelection");
 const client = require("../../db");
 require("dotenv").config();
 
 console.log(router);
-router.post("/signup", middleware.signup);
+router.post("/signup", auth.signup);
 
-router.post("/login", middleware.login);
+router.post("/login", auth.login);
 
-router.get("/verifyJWT", middleware.verifyJWT);
+router.get("/verifyJWT", auth.verifyJWT);
 
-router.post("/items", async (req, res) => {
+router.post("/items", chambers.getChambers);
+
+router.post("/bill", async (req, res) => {
   try {
-    const { items, space } = req.body;
-    const chambers = await client.query(`SELECT * FROM chambers `);
-    const commodities = await client.query(`SELECT * FROM commodities`);
-    console.log(items);
-    console.log(space);
-    let messages = [];
-    let sent = [];
-    const selectedChambers = [];
-
-    for (const item of items) {
-      for (const commodity of commodities.rows) {
-        if ((commodity.name).trim().toLowerCase() === item.trim().toLowerCase()) {
-          const selected = await client.query(`SELECT * FROM chambers WHERE chamber_id = '${commodity.chamber_id}'`);
-          console.log(selected.rows);
-          selectedChambers.push(selected.rows);
+    const temperature = Math.floor(Math.random() * (50 - 15 + 1)) + 15;
+    const humidity = Math.floor(Math.random() * (90 - 50 + 1)) + 50;
+    const { item, space, chambers, duration, cost, user_id } = req.body;
+    const commodity_id =
+      (await client.query(`SELECT commodity_id FROM commodities`)).rows.length +
+      1;
+    for (const chamber of chambers) {
+      let capacity = await client.query(
+        `SELECT capacity FROM chambers WHERE chamber_id = $1`,
+        [chamber]
+      );
+      console.log(capacity.rows);
+      try {
+        await client.query(
+          `INSERT INTO commodities(commodity_id, name, temperature, humidity, chamber_id, amount) VALUES($1, $2, $3, $4, $5, $6)`,
+          [commodity_id, item, temperature, humidity, chamber, space]
+        );
+        let commodities = await client.query(`Select commodities from chambers where chamber_id = $1`, [chamber])
+        console.log(commodities.rows[0].commodities)
+        if(!commodities.rows[0].commodities.includes(item)){
+          commodities.rows[0].commodities.push(item);
         }
-      }
-      if(selectedChambers.length === 0){
-        messages.push(`No such item found: ${item}`);
-        sent.push(chambers.rows.sort((a, b) => b.capacity - a.capacity))
-      }else{
-        messages.push(`Item found: ${item}`);
-        sent.push(selectedChambers.sort((a, b) => b.capacity - a.capacity));
+        await client.query(
+          `UPDATE chambers SET capacity = $1, user_id = $2, commodities = $3 WHERE chamber_id = $4`,
+          [capacity.rows[0].capacity - space, user_id, commodities.rows[0].commodities, chamber]
+        );
+      } catch (err) {
+        res.json(err.message);
       }
     }
 
-    console.log(selectedChambers.length, selectedChambers);
-
-    if (selectedChambers.length === 0) {
-      res.json({ message: messages, chambers: sent });
-    } else {
-      res.json({ message: messages, chambers: sent });
-    }
+    res.json({ message: "Bill created successfully" });
   } catch (error) {
-    console.error("Error:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
