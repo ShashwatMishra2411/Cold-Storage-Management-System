2cc021182793b51c299fbb1f059337970b9cf8b4
pididnothing <pranav24921@gmail.com>
2024-04-08 11:41:02 +0530

    Added Table pages and headers for both Client and Owner Dashboards

---
+++ Client/src/Pages/Tables/CChambers.jsx
@@ -0,0 +1,17 @@
+import './Tables.css';
+export default function CChambers() {
+    return (
+        <div className='back'>
+            <table>
+                <tr>
+                    <th>CID</th>
+                    <th>Capacity</th>
+                    <th>Humidity</th>
+                    <th>Temperature</th>
+                    <th>Commodities</th>
+                    <th>Coolant</th>
+                </tr>
+            </table>
+        </div>
+    )
+}
---
+++ Client/src/Pages/Tables/OFinances.jsx
@@ -0,0 +1,15 @@
+import './Tables.css';
+export default function OFinances() {
+    return (
+        <div className='back'>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Capital Investment</th>
+                    <th>Working Capital</th>
+                </tr>
+            </table>
+        </div>
+    )
+}
