92da19182b2fe5310ff56b15e703421d5417dae5
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-04-22 11:36:20 +0530

    ♻️ Refactor authentication verification and add token to commodity fetching request in CCommodities.jsx

--- Client/src/Pages/Tables/CCommodities.jsx
+++ Client/src/Pages/Tables/CCommodities.jsx
@@ -2,46 +2,86 @@ import "./Tables.css";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../Contexts/AuthContext";
+import axios from "axios";
+import { URL_ORIGIN } from "../../constants";
 export default function CCommodities() {
   const [rows, setRows] = useState([]);
   const navigate = useNavigate();
-  const { isOAuthenticated } = useAuth();
+  const { isCAuthenticated, jwtCVerify } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    if (!isOAuthenticated) {
-      console.log(isOAuthenticated);
-      navigate("/login");
+    async function checkAuthentication() {
+      try {
+        console.log("Called by Commodities");
+        await jwtCVerify();
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error verifying JWT:", error);
+        setIsLoading(false);
+      }
     }
-  }, []);
+    checkAuthentication();
+  }, [jwtCVerify]);
 
   // Simulating useEffect to receive table contents
   useEffect(() => {
-    const tableData = [
-      ["Potatoes", "1", "50", "R500", "2022-12-31"],
-      ["Tomatoes", "2", "60", "R600", "2022-12-31"],
-    ];
-    setRows(tableData);
+    async function getChambers() {
+      try {
+        const token = localStorage.getItem("token");
+        if (token) {
+          const response = await axios.get(
+            `${URL_ORIGIN}/customers/commodities`,
+            {
+              headers: {
+                Authorization: `${token}`,
+              },
+            }
+          );
+          if (response.status === 200) {
+            const data = response.data;
+            setRows(data);
+          } else {
+            console.log("Error fetching commodities");
+          }
+        } else {
+          console.log("No token found");
+        }
+      } catch (error) {
+        console.error("Error fetching commodities:", error.message);
+      }
+    }
+    getChambers();
   }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      {isOAuthenticated ? null : navigate(-1)}
+      {isCAuthenticated ? null : navigate("/login")}
       <div className="back">
         <div style={{ fontSize: "50px" }}>Commodities</div>
         <table>
-          <tr>
-            <th>Commodity</th>
-            <th>CID</th>
-            <th>Stock</th>
-            <th>Market Value</th>
-            <th>Expiration Date</th>
-          </tr>
+          <thead>
+            <tr>
+              {rows[0] &&
+                Object.keys(rows[0][0]).map((cell, index) => {
+                  return <th key={index}>{cell}</th>;
+                })}
+            </tr>
+          </thead>
           <tbody>
-            {rows.map((row, index) => (
-              <tr key={index}>
-                {row.map((cell, cellIndex) => (
-                  <td key={cellIndex}>{cell}</td>
-                ))}
-              </tr>
-            ))}
+            {rows.map((row) => {
+              return row.map((commodity, index) => {
+                return (
+                  <tr key={index}>
+                    {Object.values(commodity).map((cell, cellIndex) => {
+                      return <td key={cellIndex}>{cell}</td>;
+                    })}
+                  </tr>
+                );
+              });
+            })}
           </tbody>
         </table>
       </div>
