f86517521548a8af003f87d127abf23d1d88976a
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-04-27 13:07:13 +0530

    🐛 Fix .gitignore and update database configuration in db.js

--- Server/db.js
+++ Server/db.js
@@ -1,19 +1,41 @@
 const { Pool } = require("pg");
+const fs = require("fs");
+require('dotenv').config();
+console.log(process.env.DB_PASSWORD)
 
-const client = new Pool({
-  host: "localhost",
-  user: "postgres",
-  port: "5432",
-  password: "rootUser",
-  database: "coldstorage",
+const config = {
+    user: "avnadmin",
+    password: process.env.DB_PASSWORD,
+    host: "pg-3f48a1f0-coldstorage-89cc.d.aivencloud.com",
+    port: 14209,
+    database: "coldstorage",
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync("ca.pem").toString(),
+    },
+};
+
+const client = new Pool(config);
+client.connect(function (err) {
+    if (err)
+        throw err;
+        client.query("SELECT VERSION()", [], function (err, result) {
+          if (err)
+              throw err;
+  
+          console.log(result.rows[0].version);
+      });
 });
-
-const client1 = new Pool({
-  host: "localhost",
-  user: "postgres",
-  port: "5432",
-  password: "rootUser",
-  database: "coldstorage",
+const client1 = new Pool(config);
+client1.connect(function (err) {
+    if (err)
+        throw err;
+        client1.query("SELECT VERSION()", [], function (err, result) {
+          if (err)
+              throw err;
+  
+          console.log(result.rows[0].version);
+      });
 });
 
 module.exports = {client, client1};
